9170f34a1bb0aea4658529bd8b5d9bf0bc4ea969
softgenai <admin@softgen.ai>
2026-05-22 07:10:51 +0000

    feat(ui): integrate cookie consent banner into layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+import { CookieConsentBanner } from "@/components/CookieConsentBanner";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Toaster />
+      <CookieConsentBanner />
     </>
   );
 }
